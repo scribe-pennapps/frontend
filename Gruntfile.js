f5a79f6a168dd7cbf1c935774d102e32030f8def
Zach Latta <zach@zachlatta.com>
2014-02-14 21:59:33 -0500

    Install Foundation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -143,37 +143,33 @@ module.exports = function (grunt) {
     'bower-install': {
       app: {
         html: '<%= yeoman.app %>/index.html',
-        ignorePath: '<%= yeoman.app %>/'
+        ignorePath: '<%= yeoman.app %>/',
+        exclude: [
+          'bower_components/foundation/css/foundation.css'
+        ]
       }
     },
-
-
-
 
     // Compiles Sass to CSS and generates necessary files if requested
     compass: {
       options: {
+        config: '.compass.rb',
         sassDir: '<%= yeoman.app %>/styles',
         cssDir: '.tmp/styles',
-        generatedImagesDir: '.tmp/images/generated',
         imagesDir: '<%= yeoman.app %>/images',
         javascriptsDir: '<%= yeoman.app %>/scripts',
         fontsDir: '<%= yeoman.app %>/styles/fonts',
         importPath: '<%= yeoman.app %>/bower_components',
-        httpImagesPath: '/images',
-        httpGeneratedImagesPath: '/images/generated',
-        httpFontsPath: '/styles/fonts',
         relativeAssets: false,
-        assetCacheBuster: false,
-        raw: 'Sass::Script::Number.precision = 10\n'
       },
       dist: {
         options: {
-          generatedImagesDir: '<%= yeoman.dist %>/images/generated'
+          config: '.compass.rb'
         }
       },
       server: {
         options: {
+          config: '.compass.rb',
           debugInfo: true
         }
       }
